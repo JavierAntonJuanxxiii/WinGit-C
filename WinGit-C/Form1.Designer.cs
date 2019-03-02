fcf481ae16d1e8ab5966142e432d468f3d0302e4
JAVIER ANTON <javier.anton@juanxxiii.net>
2019-03-02 23:47:28 +0100

    AÑADO BOTON VER MENSAJE

--- WinGit-C/Form1.Designer.cs
+++ WinGit-C/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bVerMensaje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bVerMensaje
+            // 
+            this.bVerMensaje.Location = new System.Drawing.Point(84, 53);
+            this.bVerMensaje.Name = "bVerMensaje";
+            this.bVerMensaje.Size = new System.Drawing.Size(173, 95);
+            this.bVerMensaje.TabIndex = 0;
+            this.bVerMensaje.Text = "VER MENSAJE";
+            this.bVerMensaje.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bVerMensaje);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bVerMensaje;
     }
 }
 
